03189b09020b4847795c2d603162daa85628b1c2
Ihor Ruzhenko <deedeeqqs@gmail.com>
2018-08-31 15:28:59 +0300

    add/test styled-components

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+const Navigation = () => (
+    <NavDiv>
+2222
+    </NavDiv>
+);
+
+export default Navigation;
+
+const NavDiv = styled.div`
+    display: flex;
+    font-family: 'Roboto', sans-serif;
+    color: #4a4a4a;
+    font-size: 16px;
+    line-height: 19px;
+`;
+
